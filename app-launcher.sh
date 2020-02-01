4dfcc056ba3428705bdc986fdc7a6f3f69987ba4
T3SQ8 <yeydobefya@enayu.com>
2020-02-01 21:37:04 +0100

    Removed unnecessary programs

--- app-launcher.sh
+++ app-launcher.sh
@@ -1,14 +1,8 @@
 var1=$(echo "firefox
-gimp
-gnome-boxes
-keepassxc
-libreoffice
-newsboat
 ranger
-syncthing
 vim" | dmenu)
 
 case "$var1" in
-	firefox|gimp|gnome-boxes|keepassxc|libreoffice|syncthing) sh -c $var1 &;;
-	newsboat|ranger|vim ) st -e $var1 &
+	firefox) sh -c $var1 &;;
+	ranger|vim ) st -e $var1 &
 esac
