1172c33ff7856b36cd501d2716b7f2c94fe732c2
T3SQ8 <yeydobefya@enayu.com>
2020-03-28 19:33:53 +0100

    Removed package installation from deploy script and moved nessecary package names to packages-list.

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
-# Installing dependencies:
-# libxft-dev libx11-dev x11proto-dev for st.
-sudo apt install -y git xorg feh neovim ranger lemonbar dmenu make gcc bspwm sxhkd libx11-dev libxft-dev x11proto-dev acpi newsboat
-
-# Cloning repositories
-mkdir -p ~/.config/ranger ~/.config/bspwm ~/.config/sxhkd ~/.config/zsh ~/.local ~/.cache/zsh ~/.config/X11
-git clone https://github.com/T3SQ8/T3SQ8-st.git
-git clone https://github.com/T3SQ8/wallpaper.git
-cp -r wallpaper ~/.config/wallpaper
-cd T3SQ8-st
-make && sudo make install
-
-# Dotfiles
-cd ../files
+mkdir -p ~/.config/ranger ~/.config/bspwm ~/.config/sxhkd ~/.config/zsh ~/.cache/zsh ~/.config/X11 ~/.local/share/wallpaper
+cd files || echo "\"files\" directioy not found." && exit
 cp -r bin ~/.local/bin
 cp -r bspwmrc ~/.config/bspwm/bspwmrc
 cp -r rc.conf ~/.config/ranger/rc.conf
@@ -20,20 +8,6 @@ cp -r sxhkdrc ~/.config/sxhkd/sxhkdrc
 cp -r nvim ~/.config/nvim
 cp -r xinitrc ~/.config/X11/xinitrc
 cp -r newsboat ~/.config/newsboat
-
-# Configuring shell
-echo "Use BASH or ZSH?"
-echo "[1]: BASH"
-echo "[2]: ZSH"
-read -r shell
-case $shell in
-	1)
-		cp -r zprofile ~/.profile
-		;;
-	2)
-		cp -r zprofile ~/.zprofile
-		cp -r zshrc ~/.config/zsh/.zshrc
-		sudo apt install zsh
-		chsh -s /bin/zsh
-		;;
-esac
+cp -r zprofile ~/.zprofile
+cp -r zshrc ~/.config/zsh/.zshrc
+echo "Down forget to download wallpapers and install a terminal emulator."
