82e955ce88b06aa8d291411076c4108084af6502
T3SQ8 <yeydobefya@enayu.com>
2020-04-03 10:15:02 +0200

    Minor additions

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-mkdir -p ~/.config/ranger ~/.config/bspwm ~/.config/sxhkd ~/.config/zsh ~/.cache/zsh ~/.config/X11 ~/.local/share/wallpaper
 cd files || echo "\"files\" directioy not found." && exit
+mkdir -p ~/.config/ranger ~/.config/bspwm ~/.config/sxhkd ~/.config/zsh ~/.cache/zsh ~/.config/X11 ~/.local/share/wallpaper
 cp -r bin ~/.local/bin
 cp -r bspwmrc ~/.config/bspwm/bspwmrc
 cp -r rc.conf ~/.config/ranger/rc.conf
@@ -10,4 +10,4 @@ cp -r xinitrc ~/.config/X11/xinitrc
 cp -r newsboat ~/.config/newsboat
 cp -r zprofile ~/.zprofile
 cp -r zshrc ~/.config/zsh/.zshrc
-echo "Down forget to download wallpapers and install a terminal emulator."
+echo "Don't forget to download wallpapers and install a terminal emulator."
